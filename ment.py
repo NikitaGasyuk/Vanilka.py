25741bf480edeef0ba114916fc7408ebc19f02b1
NikitaGasyuk <126286242+NikitaGasyuk@users.noreply.github.com>
2023-09-19 16:22:24 +0300

    Update ment.py

--- ment.py
+++ ment.py
@@ -1,8 +1,11 @@
+import random
+
+list1 = [random.randint(-1000000, 100000) for i in range(20)]
+list2 = [random.randint(-1000000, 100000) for i in range(20)]
 def check_lists(list1, list2):
     if list1[0] == list2[0] or list1[-1] == list2[-1]:
         return True
     else:
         return False
-list1 = []
-list2 = [] 
+
 print(check_lists(list1, list2))
